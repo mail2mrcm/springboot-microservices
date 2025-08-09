2cd8c9bdf1ffe2ae27b73c2a08f77ece461d7900
mail2mrcm <118661926+mail2mrcm@users.noreply.github.com>
2025-08-09 23:44:05 +0100

    Fallback method modified to return 502 for all POST/GET API

--- api-gateway/src/main/java/com/hello2chandan/gateway/controller/FallbackController.java
+++ api-gateway/src/main/java/com/hello2chandan/gateway/controller/FallbackController.java
@@ -4,13 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class FallbackController {
 
-    @GetMapping("/systemFailure")
+    @RequestMapping(value = "/systemFailure", method = {RequestMethod.GET, RequestMethod.POST})
     public ResponseEntity<String> getSystemFailure() {
         return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("Please try after sometime");
     }
-
 }
